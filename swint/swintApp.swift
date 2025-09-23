124fa2f30a90176971677bc56deb86fcfb0fe47a
NickTheDeepest <nsemenov440@gmail.com>
2025-09-23 10:22:53 +0300

    Initial Commit

---
+++ swint/swintApp.swift
@@ -0,0 +1,17 @@
+//
+//  swintApp.swift
+//  swint
+//
+//  Created by Никита on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct swintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
